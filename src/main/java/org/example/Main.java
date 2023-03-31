b7cb65316e9d4de7285909aeda1ab9d9c2cf0a65
EWLameijer <eric_wubbo@hotmail.com>
2023-03-31 16:54:37 +0200

    Added links to other exercise sites

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
 package org.example;
 
+// need to calculate complexity... Anyway, 4x exercise copy, 1x work on UI, in necessity Tue for UI
+// https://www.w3resource.com/java-exercises/
+// https://www.idtech.com/blog/how-to-practice-java-online-with-free-coding-websites
+// https://pythonistaplanet.com/java-programming-exercises-with-solutions/?utm_content=cmp-true4
+// https://www.codezclub.com/java-solved-programs-problems-solutions/
+// https://code-exercises.com/
+// https://www.geeksforgeeks.org/java-programming-examples/7
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
