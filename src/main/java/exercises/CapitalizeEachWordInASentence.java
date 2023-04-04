d11f4a194fc92689878e4bd94dd052248f5f0a7c
EWLameijer <eric_wubbo@hotmail.com>
2023-04-04 10:41:30 +0200

    Now with properly made and annotated Uppercase Converter

--- src/main/java/exercises/CapitalizeEachWordInASentence.java
+++ src/main/java/exercises/CapitalizeEachWordInASentence.java
@@ -1,6 +1,8 @@
 package exercises;
 
-// TAGS Scanner, Scanner.nextLine(),
+// TAGS Character.toUpperCase(), Scanner, Scanner.hasNext(), Scanner.next(), Scanner.nextLine(), String.substring(),
+// String.charAt(), String.trim(),
+// string-concatenation
 
 /* DESCRIPTION
 Write a Java program to capitalize the first letter of each word in a sentence.
@@ -12,24 +14,33 @@ The Quick Brown Fox Jumps Over The Lazy Dog.
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class CapitalizeEachWordInASentence {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input a Sentence: ");
         String line = in.nextLine();
+
+        String upperCasedLine = "";
+        Scanner lineScan = new Scanner(line);
+        while (lineScan.hasNext()) {
+            String word = lineScan.next();
+            upperCasedLine += Character.toUpperCase(word.charAt(0)) + word.substring(1) + " ";
+        }
+        System.out.println(upperCasedLine.trim());
+
+        /*
+        // Another option is to use String.join on an ArrayList
         String[] words = line.split(" ");
         ArrayList<String> uppercaseWords = new ArrayList<>();
         for (String word : words) uppercaseWords.add(Character.toUpperCase(word.charAt(0)) + word.substring(1));
         System.out.println(String.join(" ", uppercaseWords));
 
-        // 7?
-        String upper_case_line = "";
-        Scanner lineScan = new Scanner(line);
-        while (lineScan.hasNext()) {
-            String word = lineScan.next();
-            upper_case_line += Character.toUpperCase(word.charAt(0)) + word.substring(1) + " ";
-        }
-        System.out.println(upper_case_line.trim());
+        // A third option is to use a stream instead of a loop:
+        var capitalized = String.join(" ", Stream.of(line.split(" ")).
+                map(w -> Character.toUpperCase(w.charAt(0)) + w.substring(1)).toList());
+        System.out.println(capitalized);
+         */
     }
 }
