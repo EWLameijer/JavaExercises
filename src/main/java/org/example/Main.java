863615b0a20bad716a673a58e84eafd628dd6716
EWLameijer <eric_wubbo@hotmail.com>
2023-04-04 10:27:08 +0200

    progress indicated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,8 @@
 package org.example;
 
 // need to calculate complexity... Anyway, 4x exercise copy, 1x work on UI, in necessity Tue for UI
-// https://www.w3resource.com/java-exercises/
+// DONE: https://codingbat.com/java
+// IN PROGRESS: https://www.w3resource.com/java-exercises/
 // https://www.idtech.com/blog/how-to-practice-java-online-with-free-coding-websites
 // https://pythonistaplanet.com/java-programming-exercises-with-solutions/?utm_content=cmp-true4
 // https://www.codezclub.com/java-solved-programs-problems-solutions/
